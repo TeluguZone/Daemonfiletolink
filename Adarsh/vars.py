a6dc31d87664800c077906432aa21111cf3e967e
TeluguZone <140744388+TeluguZone@users.noreply.github.com>
2023-11-26 10:31:39 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -35,6 +35,6 @@ class Var(object):
         URL = "https://{}/".format(FQDN)
     else:
         URL = "http://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://8te25tx:wm1YWWL7E9hSK25g@cluster0.nom13o1.mongodb.net/?retryWrites=true&w=majority'))
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://parcel:malliga@cluster0.xoax6ur.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'vj_botz'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
